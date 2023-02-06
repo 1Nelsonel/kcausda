761b8209a7dcbf54bfdbdce907b7cb24d8d28522
1Nelsonel <nelsonmokoro37@gmail.com>
2023-02-06 17:46:17 +0300

    success

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {WebView} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App
+ extends Component {
+  render() {
+    return (
+      <WebView
+        source={{
+          uri: 'https://github.com/facebook/react-native',
+        }}
+        style={{marginTop: 20}}
+      />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
